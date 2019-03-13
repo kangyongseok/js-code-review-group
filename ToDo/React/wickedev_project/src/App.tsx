b90ebc6af87a6a57b157961c68644af11ed56c0a
양정윤 <wickedev@MacBook-Pro.local>
2019-03-13 20:52:08 +0900

    [wickedev] tslint, prettier, scss, husky, lint-staged apply

--- ToDo/React/wickedev_project/src/App.tsx
+++ ToDo/React/wickedev_project/src/App.tsx
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styles from './App.module.scss'
+import logo from './logo.svg'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className={styles.container}>
+                <header className={styles.header}>
+                    <img src={logo} className={styles.logo} alt="logo" />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className={styles.link}
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
